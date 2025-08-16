f18b78f66ad8a2266213f733ba9b0419f0eebb3a
panda1835 <lehoangphuc1820@gmail.com>
2025-08-16 10:38:05 +0700

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId={process.env.GA_ID!} />
     </html>
   );
 }
